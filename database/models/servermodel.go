4f72acd9582066ab78e90d948a4b6bc2700bb72f
Adam Vlček <vlcek-adam@seznam.cz>
2025-07-27 14:45:46 +0200

    added server status caching

---
+++ database/models/servermodel.go
@@ -0,0 +1,37 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Server struct {
+	gorm.Model
+	Online      bool
+	Host        string
+	Port        string
+	LastChecked time.Time `gorm:"autoCreateTime:nano"`
+}
+
+func GetServerCache(db *gorm.DB, host string, port string) Server {
+	var server Server
+	db.Where("host = ?", host).Where("port = ?", port).First(&server)
+	if server.ID == 0 {
+		server.Host = host
+		server.Port = port
+		server.Online = false
+		server.LastChecked = time.Now()
+		db.Create(&server)
+	}
+	return server
+}
+
+func (s *Server) Update(db *gorm.DB) {
+	var dbServ Server
+	db.Where("id = ?", s.ID).First(&dbServ)
+
+	dbServ.Online = s.Online
+	dbServ.LastChecked = s.LastChecked
+	db.Save(&dbServ)
+}
