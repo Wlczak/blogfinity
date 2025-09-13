b91dd5fb3743a7e8033ef737a1c9c25f527a671d
Adam Vlček <vlcek-adam@seznam.cz>
2025-09-13 16:35:48 +0200

    finished sitemap generation

--- articles/sitemap.go
+++ articles/sitemap.go
@@ -5,29 +5,52 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/Wlczak/blogfinity/database"
+	"github.com/Wlczak/blogfinity/database/models"
+	"github.com/Wlczak/blogfinity/logger"
 	"github.com/ikeikeikeike/go-sitemap-generator/v2/stm"
 )
 
 func HandleSitemap(w http.ResponseWriter, r *http.Request) {
+	zap := logger.GetLogger()
+
 	sm := stm.NewSitemap(1)
 	sitemapDomain := os.Getenv("BASE_DOMAIN")
 	if len(sitemapDomain) == 0 {
 		http.Error(w, "Sitemap domain not set", http.StatusInternalServerError)
 		return
 	}
-	sm.Create()
 
-	fmt.Println("Generating sitemap for " + sitemapDomain)
+	db, err := database.GetDB()
+	if err != nil {
+		zap.Error(err.Error())
+	}
 
 	sm.SetDefaultHost(sitemapDomain)
+
+	sm.Create()
+
 	sm.Add(stm.URL{
 		{"loc", "/"},
+		{"changefreq", "monthly"},
+		{"priority", "1.0"},
 	})
-	// sm.Add("https://" + sitemapDomain + "/search")
-	xml := sm.Finalize().XMLContent()
+	sm.Add(stm.URL{
+		{"loc", "/search"},
+		{"changefreq", "monthly"},
+		{"priority", "0.9"},
+	})
+	articles := models.GetArticles(db, 500)
+	for article := range articles {
+		sm.Add(stm.URL{
+			{"loc", "/article/" + fmt.Sprint(articles[article].ID)},
+			{"changefreq", "monthly"},
+			{"priority", "0.6"},
+		})
+	}
 
-	fmt.Println(xml)
+	xml := sm.XMLContent()
 
-	// w.Header().Set("Content-Type", "application/xml")
-	// w.Write(xml)
+	w.Header().Set("Content-Type", "application/xml")
+	w.Write(xml)
 }
